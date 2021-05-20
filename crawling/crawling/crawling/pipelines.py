38717f5e65d5ff46a601b481210b6ab90d126ca5
madcassln <eightnum.81@gmail.com>
2021-05-20 21:04:24 +0300

    feat: store data in db

--- crawling/crawling/crawling/pipelines.py
+++ crawling/crawling/crawling/pipelines.py
@@ -1,13 +1,41 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from scrapy.utils.project import get_project_settings
+from scrapy.exceptions import DropItem
+from crawling.crawling.items import MichaelsProduct, MichaelsCategory
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+class MongoDBPipeline(object):
 
+    def __init__(self):
+        settings = get_project_settings()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.products = db[settings['MONGODB_PRODUCT_COLLECTION']]
+        self.categories = db[settings['MONGODB_CATEGORY_COLLECTION']]
 
-class CrawlingPipeline:
     def process_item(self, item, spider):
+        for data in item:
+            if not data:
+                raise DropItem("Missing data!")
+        if isinstance(item, MichaelsCategory):
+            self.__process_category_item(item)
+
+        if isinstance(item, MichaelsProduct) and item['item_id'] is not None:
+            self.__process_product_item(item)
+
         return item
+
+    def __process_category_item(self, item):
+        self.categories.update({'_id': item['_id']}, dict(item), upsert=True)
+
+    def __process_product_item(self, item):
+        product = self.products.find_one({'item_id': item['item_id']})
+        if not product:
+            self.products.insert_one(dict(item))
+            return
+        if product['category'] != item['category']:
+            product['category'] += item['category']
+        self.products.update_one({'item_id': item['item_id']}, dict(product))
